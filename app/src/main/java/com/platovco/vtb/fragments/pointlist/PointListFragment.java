db87acba9baa02bab53d89cffac6ea8e4f8531f3
Max <msaharovforwork@gmail.com>
2023-10-14 18:20:39 +0300

    Отрисовка подробной инфы по отделению

--- app/src/main/java/com/platovco/vtb/fragments/pointlist/PointListFragment.java
+++ app/src/main/java/com/platovco/vtb/fragments/pointlist/PointListFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.platovco.vtb.R;
 
@@ -20,6 +22,13 @@ public class PointListFragment extends Fragment {
 
     public static PointListFragment newInstance() {
         return new PointListFragment();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.text);
+        textView.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_pointListFragment_to_branchFragment));
     }
 
     @Override
